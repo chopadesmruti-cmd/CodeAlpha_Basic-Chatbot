46156f2cbb35daf177cdb85b999637def2077f58
chopadesmruti-cmd <chopadesmruti@gmail.com>
2025-08-21 16:04:21 +0530

    Add files via upload

---
+++ Code_Alpha_Task1.py
@@ -0,0 +1,20 @@
+# Simple Rule-Based Chatbot
+
+def chatbot():
+    print("Chatbot: Hello! Type 'bye' to exit.")
+    
+    while True:
+        user_input = input("You: ").lower()  # take input in lowercase for easy matching
+        
+        if user_input == "hello":
+            print("Chatbot: Hi!")
+        elif user_input == "how are you":
+            print("Chatbot: I'm fine, thanks!")
+        elif user_input == "bye":
+            print("Chatbot: Goodbye!")
+            break
+        else:
+            print("Chatbot: Sorry, I don't understand that.")
+
+# Run chatbot
+chatbot()
